4dbb8e06f61c329b365af0daa558e138c5497fe4
Tan Jian Hong <jianhong@sistic.com.sg>
2021-10-10 19:24:28 +0800

    Add basic frontend design

--- src/main/java/com/assignment/urlshortener/api/controller/UrlShortenApiController.java
+++ src/main/java/com/assignment/urlshortener/api/controller/UrlShortenApiController.java
@@ -6,12 +6,10 @@ import com.assignment.urlshortener.utils.ShortUrlHelper;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
-import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
+@RequestMapping(value = "/api")
 public class UrlShortenApiController {
 
     private Logger logger = LogManager.getLogger(UrlShortenApiController.class);
@@ -29,23 +27,6 @@ public class UrlShortenApiController {
         String key = urlShortenService.shortenUrl(url);
         String shortUrl = ShortUrlHelper.createShortUrl(key);
         return new ShortUrlJson(shortUrl);
-    }
-
-    @GetMapping(value = {"/s/{shorturl}"})
-    public RedirectView redirectToOriginalUrl(
-            @PathVariable(value = "shorturl", required = true) String shorturl) {
-
-        String originalUrl = urlShortenService.getOriginalUrl(shorturl);
-        if (originalUrl == null || originalUrl.isEmpty()) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Unable to find corresponding url", null);
-        } else {
-            RedirectView redirectView = new RedirectView();
-            String absoluteUrl = ShortUrlHelper.createAbsoluteUrl(originalUrl);
-            logger.info("originalUrl: " + originalUrl);
-            redirectView.setUrl(absoluteUrl);
-            redirectView.setStatusCode(HttpStatus.MOVED_PERMANENTLY);
-            return redirectView;
-        }
     }
 
 }
--- src/main/java/com/assignment/urlshortener/app/AppController.java
+++ src/main/java/com/assignment/urlshortener/app/AppController.java
@@ -1,10 +1,16 @@
 package com.assignment.urlshortener.app;
 
 import com.assignment.urlshortener.api.service.UrlShortenService;
+import com.assignment.urlshortener.utils.ShortUrlHelper;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class AppController {
@@ -18,6 +24,27 @@ public class AppController {
         this.urlShortenService = urlShortenService;
     }
 
+    @GetMapping("/")
+    public String index() {
+        logger.info("Load index()");
+        return "index";
+    }
 
+    @GetMapping(value = {"/s/{shorturl}"})
+    public RedirectView redirectToOriginalUrl(
+            @PathVariable(value = "shorturl", required = true) String shorturl) {
+
+        String originalUrl = urlShortenService.getOriginalUrl(shorturl);
+        if (originalUrl == null || originalUrl.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Unable to find corresponding url", null);
+        } else {
+            RedirectView redirectView = new RedirectView();
+            String absoluteUrl = ShortUrlHelper.createAbsoluteUrl(originalUrl);
+            logger.info("originalUrl: " + originalUrl);
+            redirectView.setUrl(absoluteUrl);
+            redirectView.setStatusCode(HttpStatus.MOVED_PERMANENTLY);
+            return redirectView;
+        }
+    }
 
 }
